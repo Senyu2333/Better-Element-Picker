9cc28a3e4173008b95547a68a438fcef2671b804
森雨Senyu <1243669851@qq.com>
2025-09-13 16:18:29 +0800

    feat-Display the DOM node's tag, class and ID.

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,11 @@
 document.addEventListener('mouseover',function (e){
-    console.log(e.target)
-})
+    const info={
+        Tag:e.target.tagName,
+        Class:e.target.className,
+        Id:e.target.id,
+    }
+    chrome.runtime.sendMessage({type:'INFO',data:info})
+    //console.log(e.target);
+    })
+
+
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,13 @@
-console.log('Popup loaded');
+chrome.runtime.onMessage.addListener(
+    function (request,sender,sendResponse){
+        const {Tag,Class,Id}=request.data
+        if(request.type==='INFO'){
+            document.getElementById('domInfo').innerHTML=
+            `
+                <p>Tag:${Tag}</p>
+                <p>Class:${Class}</p>
+                <p>ID:${Id}</p>
+            `
+        }
+    }
+)
